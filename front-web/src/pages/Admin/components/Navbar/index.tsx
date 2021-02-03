37eaf99e2705549d251657bb8813f004b1b90348
Carlos Martins <carlosemartins97@gmail.com>
2021-02-03 14:47:33 -0300

    Add Admin navbar

---
+++ front-web/src/pages/Admin/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.scss';
+
+const Navbar = () => {
+    return (
+        <nav className="admin-nav-container">
+            <ul>
+                <li>
+                    <a href="link" className="admin-nav-item">Meus Produtos</a>
+                </li>
+                <li>
+                    <a href="link" className="admin-nav-item">Minhas Categorias</a>
+                </li>
+                <li>
+                    <a href="link" className="admin-nav-item active">Meus Usuários</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
